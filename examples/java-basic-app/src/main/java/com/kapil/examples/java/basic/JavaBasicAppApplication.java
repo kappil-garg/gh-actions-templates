91e41ac5291ba2e1d0cfd761bc733ad9eaec92dd
Kapil Garg <105214446+kappil-garg@users.noreply.github.com>
2025-09-01 23:14:19 +0530

    Add basic Java example project with Hello app

---
+++ examples/java-basic-app/src/main/java/com/kapil/examples/java/basic/JavaBasicAppApplication.java
@@ -0,0 +1,13 @@
+package com.kapil.examples.java.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasicAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBasicAppApplication.class, args);
+    }
+
+}
